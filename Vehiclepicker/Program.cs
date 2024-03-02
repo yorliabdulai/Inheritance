8f4c7d809530a3757604372d1efa7cc1c6f0ccbd
yorliabdulai <yorliabdulai1@gmail.com>
2024-03-02 20:55:43 +0000

    added program entery point

--- Vehiclepicker/Program.cs
+++ Vehiclepicker/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using Vehiclepicker;
+
+class Program
+{
+    static void Main()
+    {
+        ElectricCar electricCar = new ElectricCar { Model = "Tesla", Year = 2023, Batterycapacity = 80 };
+        FuelCar fuelCar = new FuelCar { Model = "Ford", Year = 2022, FuelCapacity = 2000 };
+        int randomNumber = new Random().Next(1, 99);
+        Console.WriteLine($"Random number: {randomNumber}");
+        Console.WriteLine($"From Electric Car: {ElectricCar.PickCar(randomNumber)}");
+        Console.WriteLine($"From Fuel Car: {FuelCar.PickCar(randomNumber)}");
+    }
+}
