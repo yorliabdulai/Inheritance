a7c24de91600dbe78e8b0b1612a3c529f4a0db24
yorliabdulai <yorliabdulai1@gmail.com>
2024-03-02 20:14:52 +0000

    first commit

---
+++ Vehiclepicker/Vehicle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehiclepicker
+{
+    public class Vehicle
+    {
+        public string Model { get; set; }
+        public int Year { get; set; }
+    }
+    public virtual void PickCar(int number)
+    {
+        if(number % 2 == 0)
+        {
+            Console.WriteLine($"Picked an Electric car - Model: {Model}, Year: {Year}");
+        }else
+        {
+            Console.WriteLine($"Picked a fuel car - Model: {Model}, Year: {year}")
+        }
+    }
+}
